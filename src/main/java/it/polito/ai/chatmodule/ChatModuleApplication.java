f05fc3c3661781fda79dabd972d3f6f61f66f60b
MicheledG <chiefzephyr@gmail.com>
2017-06-10 10:11:23 +0200

    initial commit

---
+++ src/main/java/it/polito/ai/chatmodule/ChatModuleApplication.java
@@ -0,0 +1,12 @@
+package it.polito.ai.chatmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatModuleApplication.class, args);
+	}
+}
