73fb5c846e521042125f4f57a9884fcafda6f496
Francesco Longo <france193.htc@gmail.com>
2017-06-30 16:46:06 +0200

    - added @Repository

--- src/main/java/it/polito/ai/chatmodule/repositories/messages/BikeTripMessageRepository.java
+++ src/main/java/it/polito/ai/chatmodule/repositories/messages/BikeTripMessageRepository.java
@@ -3,10 +3,12 @@ package it.polito.ai.chatmodule.repositories.messages;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import it.polito.ai.chatmodule.model.messages.BikeTripMessage;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by france193 on 24/06/2017.
  */
+@Repository
 public interface BikeTripMessageRepository extends MongoRepository<BikeTripMessage, String> {
     //@Query(value = "{}", fields = "{}")
     //public List<BikeTripMessage> findBikeTripMessages(Pageable pageable);
--- src/main/java/it/polito/ai/chatmodule/repositories/messages/BusMetroMessageRepository.java
+++ src/main/java/it/polito/ai/chatmodule/repositories/messages/BusMetroMessageRepository.java
@@ -3,10 +3,13 @@ package it.polito.ai.chatmodule.repositories.messages;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import it.polito.ai.chatmodule.model.messages.BusMetroMessage;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by france193 on 24/06/2017.
  */
+
+@Repository
 public interface BusMetroMessageRepository extends MongoRepository<BusMetroMessage, String> {
     //@Query(value = "{}", fields = "{}")
     //public List<BusMetroMessage> findBusMetroMessages(Pageable pageable);
