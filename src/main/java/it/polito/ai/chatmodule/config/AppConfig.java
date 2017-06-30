a322d2629f5643900703053cfe2eecc57459babf
MicheledG <chiefzephyr@gmail.com>
2017-06-30 16:42:05 +0200

    Added app configuration

---
+++ src/main/java/it/polito/ai/chatmodule/config/AppConfig.java
@@ -0,0 +1,10 @@
+package it.polito.ai.chatmodule.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "it.polito.ai")
+public class AppConfig {
+
+}
